4d8432c019021036e5592a60438c8d4f4752882e
JikaiSH <79759440+JikaiSH@users.noreply.github.com>
2022-04-07 04:46:27 +0200

    master

--- myself.js
+++ myself.js
@@ -28,11 +28,14 @@ console.log('https://yourwebsite.here')
     //  x.style.display = "none";
    // }
     document.getElementById("innerdiv").innerHTML = "I and yes an other boring test i test it for learn JS (aka JavaScript) " + 10 * 1
+    document.getElementById("title").innerHTML = `<img src="https:\/\/external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.freebiesupply.com%2Flogos%2Fthumbs%2F2x%2Fjavascript-logo.png&f=1&nofb=1" width="70px" height="50px"> Test !`
    if(document.getElementById("testing").innerHTML === "Return me") {
     document.getElementById("testing").innerHTML = "Change me";
+    document.getElementById("title").innerHTML = `<img src="https:\/\/external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.freebiesupply.com%2Flogos%2Fthumbs%2F2x%2Fjavascript-logo.png&f=1&nofb=1" width="70px" height="50px"> Test !`
    }
    else {
-    document.getElementById("testing").innerHTML = "Return me"
+    document.getElementById("title").innerHTML = `<img src="https:\/\/external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.freebiesupply.com%2Flogos%2Fthumbs%2F2x%2Fjavascript-logo.png&f=1&nofb=1" width="70px" height="50px"> Testing !`
     document.getElementById("innerdiv").innerHTML = "And boom the text changed " + 10 * 2
+    document.getElementById("testing").innerHTML = "Return me"
    }
   }
