8692c1b2f6ed022c0adcbd1bd589b430b6f037d7
JikaiSH <79759440+JikaiSH@users.noreply.github.com>
2022-04-08 01:33:38 +0200

    renamed and changed some codes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,6 @@
-//Can't change information
-const username = "Your name" //can't change
-const country = "Your Country"
-const gender = "Your gender" //can't change
-const sexuality = "Your sexuality" //can't change
-const taken = "Are you in couple ?" //can('t) change
-
-//Can change information
-let age = "Your age" //can change
-let language = "Language you speak " // + "Second language"
-
-console.log('%cHi! i\'\am ' + username,"color:red;font-size: 50px;")
-console.log('Im frome ' + country)
-console.log('I have ' + age + 'y/o')
-console.log('I speak ' + language)
-console.log('Im a ' + gender)
-console.log('Im ' + sexuality)
-console.log('Im ' + taken)
-console.log('https://yourwebsite.here')
-
   function showtext() {
-    var x = document.getElementById("innerdiv");
+    //for only hide text
+    //var x = document.getElementById("innerdiv");
     const dStyle = document.querySelector('style');
     //for only hide text
    // if (x.style.display === "none") {
@@ -45,3 +26,23 @@ console.log('https://yourwebsite.here')
     document.getElementById("testing").innerHTML = "Launch Server"
    }
   }
+
+  //Can't change information
+//const username = "Your name" //can't change
+//const country = "Your Country"
+//const gender = "Your gender" //can't change
+//const sexuality = "Your sexuality" //can't change
+//const taken = "Are you in couple ?" //can('t) change
+
+//Can change information
+//let age = "Your age" //can change
+//let language = "Language you speak " // + "Second language"
+
+//console.log('%cHi! i\'\am ' + username,"color:red;font-size: 50px;")
+//console.log('Im frome ' + country)
+//console.log('I have ' + age + 'y/o')
+//console.log('I speak ' + language)
+//console.log('Im a ' + gender)
+//console.log('Im ' + sexuality)
+//console.log('Im ' + taken)
+//console.log('https://yourwebsite.here')
